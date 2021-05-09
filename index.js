63210da01876fc35e68e854b303a9ad298c43af2
Rosa <rosarenee@att.net>
2021-05-09 02:43:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove(); //.remove();
+const newHeader = document.createElement('h1') // document.createElement('ul');
+document.body.appendChild(newHeader); //document.body.appendChild(element); 
+newHeader.id= "victory";  //give div id
+victory.innerHTML = "YOUR-NAME is the champion"; //make victory say so like maybe <p>bs
